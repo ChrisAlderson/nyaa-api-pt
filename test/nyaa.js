ff4ebad77430bc14b87c3ff32d817b083b9de027
Chris <chrisalderson@protonmail.com>
2017-07-18 00:35:12 +0000

    move to standard

--- test/nyaa.js
+++ test/nyaa.js
@@ -1,15 +1,20 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
-const assert = chai.assert;
-const NyaaAPI = require('../nyaa-api-pt');
+const { expect } = require('chai')
+const NyaaAPI = require('../nyaa-api-pt')
 
 describe('Nyaa', () => {
 
-  let nyaa;
-  before(() => nyaa = new NyaaAPI());
+  let nyaa
 
-  it('Should get results with a search', done => {
+  before(() => {
+    console.warn = () => {}
+    nyaa = new NyaaAPI({
+      debug: true
+    })
+  })
+
+  it('should search for torrents', done => {
     nyaa.search({
       query: 'horriblesubs',
       filter: 'trusted_only',
@@ -17,11 +22,76 @@ describe('Nyaa', () => {
       sub_category: 'english_translated',
       page: 2,
       sort: 'date',
-      order: 'asc'
+      order: false,
+      max: 300,
+      userID: 14
     }).then(res => {
-      assert.isArray(res.results);
-      done();
-    }).catch(err => done(err));
-  });
+      expect(res).to.be.an('array')
+      expect(res.length).to.be.at.least(1)
 
-});
+      done()
+    }).catch(done)
+  })
+
+  it('should throw an error when searching for torrents', () => {
+    // expect(nyaa.search.bind(nyaa.search, {
+    //   filter: 'failing'
+    // })).to.throw('failing is an invalid option for filter!')
+    expect(nyaa.search.bind(nyaa.search, {
+      sort: 'failing'
+    })).to.throw('failing is an invalid option for sort!')
+    expect(nyaa.search.bind(nyaa.search, {
+      category: 'failing'
+    })).to.throw('failing is an invalid option for category!')
+    expect(nyaa.search.bind(nyaa.search, {
+      subCategory: 'failing'
+    })).to.throw('failing')
+
+    expect(nyaa.search.bind(nyaa.search, {
+      category: null,
+      subCategory: 'failing'
+    })).to.throw('Sub category needs a category!')
+  })
+
+  it('should get torrents from a page', done => {
+    nyaa.getTorrents({
+      max: 50,
+      page: 1
+    }).then(res => {
+      expect(res.torrents).to.be.an('array')
+      expect(res.torrents.length).to.be.at.least(1)
+      expect(res.queryRecordCount).to.be.a('number')
+      expect(res.totalRecordCount).to.be.a('number')
+
+      done()
+    }).catch(done)
+  })
+
+  it('should get details on a torrent', done => {
+    nyaa.getTorrent(924727).then(res => {
+      expect(res.id).to.be.a('number')
+      expect(res.name).to.be.a('string')
+      expect(res.hash).to.be.a('string')
+      expect(res.magnet).to.be.a('string')
+
+      done()
+    }).catch(done)
+  })
+
+  // it('should upload a torrent', () => {
+  //   nyaa.upload({
+  //     name,
+  //     category,
+  //     subCategory,
+  //     description,
+  //     hash
+  //   }).then(res => {
+  //     done()
+  //   }).catch(done)
+  // })
+  //
+  // it('should update a torrent', () => {
+  //
+  // })
+
+})
