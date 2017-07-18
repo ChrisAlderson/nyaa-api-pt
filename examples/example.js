675ba9b01635f33b8b8ee14c5e8d4ba5ab693535
Chris <chrisalderson@protonmail.com>
2017-07-18 00:34:30 +0000

    moved to standard

--- examples/example.js
+++ examples/example.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
 // Import the API wrapper.
-const NyaaAPI = require('../nyaa-api-pt');
+const NyaaAPI = require('../nyaa-api-pt')
 
 // Create an instance of the API wrapper.
-const nyaa = new NyaaAPI({debug: true});
+const nyaa = new NyaaAPI()
 
-// Make a search request to nyaa.se.
+// Make a search request to nyaa.pantsu.cat:
 nyaa.search({
   query: 'horriblesubs',
   filter: 'trusted_only',
@@ -14,7 +14,20 @@ nyaa.search({
   sub_category: 'english_translated',
   page: 2,
   sort: 'date',
-  order: 'asc'
-})
-.then(res => console.log(res))
-  .catch(err => console.error(err));
+  order: false,
+  max: 300,
+  userID: 14
+}).then(res => console.log(res))
+  .catch(err => console.error(err))
+
+// Make a get request to nyaa.pantsu.cat:
+nyaa.getTorrents({
+  max: 50,
+  page: 1
+}).then(res => console.log(res))
+  .catch(err => console.error(err))
+
+// Get details on a torrent.
+nyaa.getTorrent(924727)
+  .then(res => console.log(res))
+  .catch(err => console.error(err))
--- nyaa-api-pt.js
+++ nyaa-api-pt.js
@@ -1,191 +1,185 @@
-'use strict';
+'use strict'
 
-const cheerio = require('cheerio');
-const querystring = require('querystring');
-const got = require('got');
+const querystring = require('querystring')
+const got = require('got')
 
 module.exports = class NyaaAPI {
 
-  constructor({baseUrl = 'https://nyaa.si', debug = false} = {}) {
-    NyaaAPI._baseUrl = baseUrl;
-    this._debug = debug;
+  constructor({
+    baseUrl = 'https://nyaa.pantsu.cat',
+    apiToken = '',
+    debug = false
+  } = {}) {
+    this._baseUrl = baseUrl
+    this._apiToken = apiToken
+    this._debug = debug
 
-    this._categories = {
+    NyaaAPI._categories = {
       all_categories: {
-        category_id: '0_0'
+        categoryId: '_'
       },
       anime: {
-        category_id: '1_0',
-        sub_categories: {
-          anime_music_video: '1_1',
-          english_translated: '1_2',
-          non_english_translated: '1_3',
-          raw: '1_4'
+        categoryId: '3_',
+        subCategories: {
+          anime_music_video: '3_12',
+          english_translated: '3_5',
+          non_english_translated: '3_13',
+          raw: '3_6'
         }
       },
       audio: {
-        category_id: '2_0',
-        sub_cats: {
-          lossless: '2_1',
-          lossy: '2_2'
+        categoryId: '2_',
+        subCategories: {
+          lossless: '2_3',
+          lossy: '2_4'
         }
       },
       literature: {
-        category_id: '3_0',
-        sub_categories: {
-          english_translated: '3_1',
-          non_english_translated: '3_2',
-          raw: '3_3'
+        categoryId: '4_',
+        subCategories: {
+          english_translated: '4_7',
+          raw: '4_8',
+          non_english_translated: '4_14'
         }
       },
       live_action: {
-        category_id: '4_0',
-        sub_categories: {
-          english_translated: '4_1',
-          idol_promotional_video: '4_2',
-          non_english_translated: '4_3',
-          raw: '4_4',
+        categoryId: '5_',
+        subCategories: {
+          english_translated: '5_9',
+          idol_promotional_video: '5_10',
+          non_english_translated: '5_18',
+          raw: '5_11'
         }
       },
       pictures: {
-        category_id: '5_0',
-        sub_categories: {
-          graphics: '5_1',
-          photos: '5_2'
+        categoryId: '6_',
+        subCategories: {
+          graphics: '6_15',
+          photos: '6_16'
         }
       },
       software: {
-        category_id: '6_0',
-        sub_categories: {
-          applications: '6_1',
-          games: '6_2'
+        categoryId: '1_',
+        subCategories: {
+          applications: '1_1',
+          games: '1_2'
         }
       }
-    };
-
-    this._filters = {
-      no_filter: 0,
+    }
+    NyaaAPI._filters = {
+      show_all: 0,
       filter_remakes: 1,
       trusted_only: 2,
       a_only: 3
-    };
-
-    this._sorters = {
-      size: 'size',
-      date: 'id',
-      seeders: 'seeders',
-      leechers: 'leechers',
-      downloads: 'downloads'
-    };
-    this._orders = {
-      asc: 'asc',
-      desc: 'desc'
-    };
+    }
+    NyaaAPI._sorters = {
+      name: 1,
+      seeders: 5,
+      downloads: 6,
+      leechers: 7,
+      date: 2,
+      size: 4
+    }
   }
 
-  _get(data = {}) {
-    if (this._debug)
-      console.warn(`Making request to: '${NyaaAPI._baseUrl}', opts: ${querystring.stringify(data)}`);
+  _request(method, uri, data = {}) {
+    if (this._debug) {
+      console.warn(`Making request to: '${this._baseUrl}/${uri}/${querystring.stringify(data)}`)
+    }
 
-    return got(NyaaAPI._baseUrl, {
-      method: 'GET',
-      query: data
-    }).then(({body}) => cheerio.load(body));
+    return got(`${this._baseUrl}/${uri}`, {
+      method,
+      headers: {
+        authorization: this._apiToken
+      },
+      json: true,
+      body: data
+    }).then(({body}) => body)
   }
 
-  _requestData({ filter = 'no_filter', category = 'all_categories', sub_category, query = '', page = 0, sort, order = 'desc' }) {
-    if (filter && !this._filters[filter])
-      throw new Error(`${filter} is an invalid option for filter!`);
+  search({
+    filter = 'show_all',
+    category = 'all_categories',
+    subCategory,
+    query = '',
+    page = 1,
+    sort,
+    order = false,
+    max = 50,
+    userID
+  } = {}) {
+    // if (filter && NyaaAPI._filters[filter] === 0 && NyaaAPI._filters[filter]) {
+    //   throw new Error(`${filter} is an invalid option for filter!`) // TODO: test this line.
+    // }
 
-    if (category && !this._categories[category])
-      throw new Error(`${category} is an invalid option for category!`);
+    if (sort && !NyaaAPI._sorters[sort]) {
+      throw new Error(`${sort} is an invalid option for sort!`)
+    }
 
-    if (sort && !this._sorters[sort])
-      throw new Error(`${sort} is an invalid option for sort!`);
+    if (category && !NyaaAPI._categories[category]) {
+      throw new Error(`${category} is an invalid option for category!`)
+    }
 
-    if (!category && sub_category)
-      throw new Error('Sub category needs a category!');
+    if (!category && subCategory) {
+      throw new Error('Sub category needs a category!')
+    }
 
-    if (category && sub_category && (!this._categories[category] || !this._categories[category].sub_categories[sub_category]))
-      throw new Error(`${category} is an invalid option for category or ${sub_category} is an invalid option for sub_category!`);
+    if (category && subCategory && (!NyaaAPI._categories[category] ||
+        !NyaaAPI._categories[category].subCategories[subCategory])) {
+      throw new Error(`${category} is an invalid option for category or ${subCategory} is an invalid option for subCategory!`) // TODO: test this line.
+    }
 
     const qs = {
       q: query,
-      f: this._filters[filter],
-      p: page,
-      s: this._sorters[sort],
+      s: NyaaAPI._filters[filter],
+      max,
+      userID,
+      sort: NyaaAPI._sorters[sort],
       o: order
-    };
-
-    if (category && sub_category) {
-      qs.c = this._categories[category].sub_categories[sub_category]
-    } else if (category) {
-      qs.c = this._categories[category].category_id;
     }
 
-    return this._get(qs);
+    if (category && subCategory) {
+      qs.c = NyaaAPI._categories[category].subCategories[subCategory] // TODO: test this line.
+    } else if (category) {
+      qs.c = NyaaAPI._categories[category].category_id
+    }
+
+    return this._request('GET', `api/search/${page}`, qs)
   }
 
-  _formatData($) {
-    const result = {
-      /**
-       * TODO: Currently the site is limited to 1000 torrents (14 pages) when
-       * the query option is used. `total_pages` could be replaced with a
-       * `hasMore` property.
-       */
-      total_pages: 1,
-      results: []
-    };
-
-    $('tr.success').each(function(element) {
-      const entry = $(this).find('td');
-
-      // TODO: add categories to results.
-      // const categoryRegex = /(\w+)\s+-\s+(.*)/;
-      // const completeCategory = entry.eq(0).find('a')
-                                                // .attr('title');
-      // const category = completeCategory.match(categoryRegex)[1];
-      // const sub_category = completeCategory.match(categoryRegex)[2];
-
-      const title = entry.eq(1).find('a').text();
-      const link = `${NyaaAPI._baseUrl}${entry.eq(1).find('a').attr('href')}`;
-      const torrent_link = `${NyaaAPI._baseUrl}${entry.eq(2).find('a').eq(0).attr('href')}`;
-      const magnet = entry.eq(2).find('a').eq(1).attr('href');
-      const size = entry.eq(3).text();
-      const date = new Date(entry.eq(4).text());
-      const seeders = parseInt(entry.eq(5).text(), 10);
-      const leechers = parseInt(entry.eq(6).text(), 10);
-      const peers = seeders + leechers;
-      const downloads = parseInt(entry.eq(7).text(), 10);
-
-      result.results.push({
-        // category,
-        // sub_category,
-        title,
-        link,
-        torrent_link,
-        magnet,
-        size,
-        seeders,
-        leechers,
-        peers,
-        downloads
-      });
-    });
-
-    return result;
+  getTorrents({page = 1, max = 50} = {}) {
+    return this._request('GET', `api/${page}`, { max })
   }
 
-  search({ filter, category, sub_category, query, page, sort, order }) {
-    return this._requestData({
-      filter,
-      category,
-      sub_category,
-      query,
-      page,
-      sort,
-      order
-    }).then(this._formatData);
+  getTorrent(id) {
+    return this._request('GET', `api/view/${id}`)
   }
+
+  // upload({name, category, subCategory, description, hash}) {
+  //   if (category && !NyaaAPI._categories[category]) {
+  //     throw new Error(`${category} is an invalid option for category!`)
+  //   }
+  //
+  //   if (!category && subCategory) {
+  //     throw new Error('Sub category needs a category!')
+  //   }
+  //
+  //   if (category && subCategory && (!NyaaAPI._categories[category] ||
+  //       !NyaaAPI._categories[category].sub_categories[subCategory])) {
+  //     throw new Error(`${category} is an invalid option for category or ${subCategory} is an invalid option for subCategory!`)
+  //   }
+  //
+  //   return this._request('POST', '/api/upload', {
+  //     name,
+  //     category,
+  //     sub_category: subCategory,
+  //     description,
+  //     hash
+  //   })
+  // }
+
+  // update() {
+  //   return this._request('PUT', '/api/update')
+  // }
 
 }
