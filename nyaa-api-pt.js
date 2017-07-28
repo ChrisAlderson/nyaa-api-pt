2df3ff13f40c2894d7dd031cf2485c5cd901f682
Chris Alderson <chrisalderson@protonmail.com>
2017-07-28 17:24:43 +0200

    still a wip

--- nyaa-api-pt.js
+++ nyaa-api-pt.js
@@ -1,185 +1,421 @@
 'use strict'
 
-const querystring = require('querystring')
+// Import the necessary modules.
 const got = require('got')
+const { stringify } = require('querystring')
 
-module.exports = class NyaaAPI {
+/**
+ * The torrent model.
+ * @typedef {Object} Torrent
+ * @property {number} id ID of the torrent.
+ * @property {string} name Name of the torrent.
+ * @property {number} status Status of the torrent.
+ * @property {string} hash Hash of the torrent.
+ * @property {string} date Uploaded date of the torrent.
+ * @property {number} filesize File size in Bytes of the torrent.
+ * @property {string} description Description of the torrent.
+ * @property {Array<Object>} comments Comments of the torrent.
+ * @property {string} sub_category Sub Category of the torrent.
+ * @property {string} category Category of the torrent.
+ * @property {string} anidb_id Anidb ID of the torrent.
+ * @property {number} uploader_id ID of the torrent uploader.
+ * @property {string} uploader_name  Username of the torrent uploader.
+ * @property {string} uploader_old  Old username from nyaa of the torrent
+ * uploader.
+ * @property {string} website_link  External Link of the torrent.
+ * @property {Array<String>} languages  Languages of the torrent.
+ * @property {string} magnet  Magnet URI of the torrent.
+ * @property {string} torrent  Download URL of the torrent.
+ * @property {number} seeders  Number of seeders of the torrent.
+ * @property {number} leechers  Number of leechers of the torrent.
+ * @property {number} completed  Downloads completed of the torrent.
+ * @property {string} last_scrape  Last statistics update of the torrent.
+ * @property {Array<Object>} file_list  List of files in the torrent.
+ */
 
+/**
+ * The upload and update response model.
+ * @typedef {Object} UploadUpdateResponse
+ * @property {Boolean} ok The request is done without failing.
+ * @property {String[]} infos Messages information relative to the request
+ * @property {Torrent} data The resulting torrent uploaded.
+ */
+
+/**
+ * The query response model.
+ * @typedef {Object} QueryResponse
+ * @property {Array<Torrent>} torrents List of torrent object.
+ * @property {number} queryRecordCount Number of torrents given.
+ * @property {number} totalRecordCount Total number of torrents.
+ */
+
+/**
+ * The profile model.
+ * @typedef {Object} Profile
+ * @property {Boolean} ok The request is done without failing.
+ * @property {String[]} infos Messages information relative to the request.
+ * @property {User} data The connected user object.
+ */
+
+/**
+ * The user model.
+ * @typedef {Object} User
+ * @property {number} user_id The id of the user.
+ * @property {string} username The username of the user.
+ * @property {number} status The status of the user.
+ * @property {string} token The token of the user.
+ * @property {string} md5 The md5 hash of the user.
+ * @property {Date} created_at The time the user was created.
+ * @property {number} liking_count The likes of the user.
+ * @property {number} liked_count The amount of likes the user has given.
+ */
+
+/**
+ * A nyaa.pantsu.cat API wrapper for NodeJS
+ * @type {NyaaApi}
+ */
+module.exports = class NyaaApi {
+
+  /**
+   * Create a new instance of the module.
+   * @param {!Object} config={} - The configuration object for the module.
+   * @param {!string} baseUrl=https://nyaa.pantsu.cat/ - The base url of nyaa.pantsu.
+   * @params {!string} apiToken - Your API token.
+   * @param {?boolean} [debug=false] - Show extra output.
+   */
   constructor({
-    baseUrl = 'https://nyaa.pantsu.cat',
-    apiToken = '',
+    baseUrl = 'https://nyaa.pantsu.cat/',
+    apiToken,
     debug = false
   } = {}) {
+    /**
+     * The base url of nyaa.pantsu.
+     * @type {string}
+     */
     this._baseUrl = baseUrl
+
+    /**
+     * Your API token.
+     * @type {string}
+     */
     this._apiToken = apiToken
+
+    /**
+     *  Show extra output.
+     * @type {string}
+     */
     this._debug = debug
 
-    NyaaAPI._categories = {
-      all_categories: {
-        categoryId: '_'
-      },
-      anime: {
-        categoryId: '3_',
-        subCategories: {
-          anime_music_video: '3_12',
-          english_translated: '3_5',
-          non_english_translated: '3_13',
-          raw: '3_6'
-        }
-      },
-      audio: {
-        categoryId: '2_',
-        subCategories: {
-          lossless: '2_3',
-          lossy: '2_4'
-        }
-      },
-      literature: {
-        categoryId: '4_',
-        subCategories: {
-          english_translated: '4_7',
-          raw: '4_8',
-          non_english_translated: '4_14'
-        }
-      },
-      live_action: {
-        categoryId: '5_',
-        subCategories: {
-          english_translated: '5_9',
-          idol_promotional_video: '5_10',
-          non_english_translated: '5_18',
-          raw: '5_11'
-        }
-      },
-      pictures: {
-        categoryId: '6_',
-        subCategories: {
-          graphics: '6_15',
-          photos: '6_16'
-        }
-      },
-      software: {
-        categoryId: '1_',
-        subCategories: {
-          applications: '1_1',
-          games: '1_2'
-        }
-      }
-    }
-    NyaaAPI._filters = {
-      show_all: 0,
-      filter_remakes: 1,
-      trusted_only: 2,
-      a_only: 3
-    }
-    NyaaAPI._sorters = {
+    /**
+     * The sorters for searching.
+     * @type {Object}
+     */
+    this._sorters = {
+      id: 0,
       name: 1,
-      seeders: 5,
-      downloads: 6,
-      leechers: 7,
       date: 2,
-      size: 4
+      downloads: 3,
+      size: 4,
+      seeders: 5,
+      leechers: 6,
+      completed: 7
     }
   }
 
-  _request(method, uri, data = {}) {
+  /**
+   * Send a HTTP request to the API of nyaa.pantsu.
+   * @param {!string} method - The method of the of HTTP.
+   * @param {!string} endpoint - The endpoint to send the request to.
+   * @param {?Object} query - The query to send with the request.
+   * @returns {Promise<Object, Error>} - The promise to send a HTTP request.
+   */
+  _request(method, endpoint, query) {
+    const uri = `${this._baseUrl}${endpoint}`
+
     if (this._debug) {
-      console.warn(`Making request to: '${this._baseUrl}/${uri}/${querystring.stringify(data)}`)
+      console.warn(`Making request to: '${uri}?${stringify(query)}'`)
     }
 
-    return got(`${this._baseUrl}/${uri}`, {
+    return got(uri, {
       method,
       headers: {
         authorization: this._apiToken
       },
       json: true,
-      body: data
+      form: true,
+      body: query
     }).then(({body}) => body)
   }
 
-  search({
-    filter = 'show_all',
-    category = 'all_categories',
-    subCategory,
-    query = '',
-    page = 1,
-    sort,
-    order = false,
-    max = 50,
-    userID
-  } = {}) {
-    // if (filter && NyaaAPI._filters[filter] === 0 && NyaaAPI._filters[filter]) {
-    //   throw new Error(`${filter} is an invalid option for filter!`) // TODO: test this line.
-    // }
-
-    if (sort && !NyaaAPI._sorters[sort]) {
-      throw new Error(`${sort} is an invalid option for sort!`)
-    }
-
-    if (category && !NyaaAPI._categories[category]) {
-      throw new Error(`${category} is an invalid option for category!`)
-    }
-
-    if (!category && subCategory) {
-      throw new Error('Sub category needs a category!')
-    }
-
-    if (category && subCategory && (!NyaaAPI._categories[category] ||
-        !NyaaAPI._categories[category].subCategories[subCategory])) {
-      throw new Error(`${category} is an invalid option for category or ${subCategory} is an invalid option for subCategory!`) // TODO: test this line.
-    }
-
-    const qs = {
-      q: query,
-      s: NyaaAPI._filters[filter],
-      max,
-      userID,
-      sort: NyaaAPI._sorters[sort],
-      o: order
-    }
-
-    if (category && subCategory) {
-      qs.c = NyaaAPI._categories[category].subCategories[subCategory] // TODO: test this line.
-    } else if (category) {
-      qs.c = NyaaAPI._categories[category].category_id
-    }
-
-    return this._request('GET', `api/search/${page}`, qs)
+  /**
+   * Send a HTTP GET request to the API of nyaa.pantsu.
+   * @param {!string} endpoint - The endpoint to send the GET request to.
+   * @param {?Object} query - The query to send with the GET request.
+   * @returns {Promise<Object, Error>} - The promise to send a HTTP GET
+   * request.
+   */
+  _get(endpoint, query) {
+    return this._request('GET', endpoint, query)
   }
 
-  getTorrents({page = 1, max = 50} = {}) {
-    return this._request('GET', `api/${page}`, { max })
+  /**
+   * Send a HTTP HEAD request to the API of nyaa.pantsu.
+   * @param {!string} endpoint - The endpoint to send the HEAD request to.
+   * @param {?Object} query - The query to send with the HEAD request.
+   * @returns {Promise<Object, Error>} - The promise to send a HTTP HEAD
+   * request.
+   */
+  _head(endpoint, query) {
+    return this._request('HEAD', endpoint, query)
   }
 
+  /**
+   * Send a HTTP POST request to the API of nyaa.pantsu.
+   * @param {!string} method - The method of the of HTTP.
+   * @param {!string} endpoint - The endpoint to send the POST request to.
+   * @param {?Object} query - The query to send with the POST request.
+   * @returns {Promise<Object, Error>} - The promise to send a HTTP POST
+   * request.
+   */
+  _post(endpoint, query) {
+    return this._request('POST', endpoint, query)
+  }
+
+  /**
+   * Send a HTTP PUT request to the API of nyaa.pantsu.
+   * @param {!string} endpoint - The endpoint to send the PUT request to.
+   * @param {?Object} query - The query to send with the PUT request.
+   * @returns {Promise<Object, Error>} - The promise to send a HTTP PUT request.
+   */
+  _put(endpoint, query) {
+    return this._request('PUT', endpoint, query)
+  }
+
+  /**
+   * Get a torrent by the given id.
+   * @param {number} id Torrent unique ID.
+   * @return {Promise<Torrent, Error>} - The promise to get a torrent.
+   */
   getTorrent(id) {
-    return this._request('GET', `api/view/${id}`)
+    return this._get(`api/view/${id}`)
   }
 
-  // upload({name, category, subCategory, description, hash}) {
-  //   if (category && !NyaaAPI._categories[category]) {
-  //     throw new Error(`${category} is an invalid option for category!`)
-  //   }
-  //
-  //   if (!category && subCategory) {
-  //     throw new Error('Sub category needs a category!')
-  //   }
-  //
-  //   if (category && subCategory && (!NyaaAPI._categories[category] ||
-  //       !NyaaAPI._categories[category].sub_categories[subCategory])) {
-  //     throw new Error(`${category} is an invalid option for category or ${subCategory} is an invalid option for subCategory!`)
-  //   }
-  //
-  //   return this._request('POST', '/api/upload', {
-  //     name,
-  //     category,
-  //     sub_category: subCategory,
-  //     description,
-  //     hash
-  //   })
+  /**
+   * TODO: add description.
+   * @param {number} id Torrent unique ID.
+   * @return {Promise<Torrent, Error>} - The promise to get a torrent.
+   */
+  getTorrentHead(id) {
+    return this._head(`api/view/${id}`)
+  }
+
+  /**
+   * Upload a new torrent.
+   * @param {string} username - Torrent uploader name.
+   * @param {string} name - Torrent name.
+   * @param {string} magnet - Torrent magnet URI.
+   * @param {string} category - Torrent category.
+   * @param {boolean} remake - Torrent is a remake.
+   * @param {string} description - Torrent description.
+   * @param {number} status - Torrent status.
+   * @param {boolean} hidden - Torrent hidden.
+   * @param {string} websiteLink - Torrent website link.
+   * @param {Array<string>} languages - Torrent languages.
+   * @param {String} torrent - Torrent file to upload (you have to send a
+   * torrent file or a magnet, not both!).
+   * @returns {Promise<UploadUpdateResponse, Error} - The promise to upload a
+   * torrent.
+  */
+  uploadTorrent({
+    username,
+    name,
+    magnet,
+    category,
+    remake,
+    description,
+    status,
+    hidden,
+    websiteLink,
+    languages,
+    torrent
+  }) {
+    // TODO: test this method.
+    return this._post('api/upload', {
+      username,
+      name,
+      magnet,
+      category,
+      remake,
+      description,
+      status,
+      hidden,
+      website_link: websiteLink,
+      languages,
+      torrent
+    })
+  }
+
+  /**
+   * Update an existing torrent.
+   * @param {string} username - Torrent uploader name.
+   * @param {number} id - Torrent ID.
+   * @param {string} name - Torrent name.
+   * @param {string} category - Torrent category.
+   * @param {boolean} remake - Torrent is a remake.
+   * @param {string} description - Torrent description.
+   * @param {number} status - Torrent status.
+   * @param {boolean} hidden - Torrent hidden.
+   * @param {string} websiteLink - Torrent website link.
+   * @param {Array<string>} languages - Torrent languages.
+   * @returns {Promise<UploadUpdateResponse, Error} - The promise to update a
+   * torrent.
+  */
+  updateTorrent({
+    username,
+    id,
+    name,
+    category,
+    remake,
+    description,
+    status,
+    hidden,
+    websiteLink,
+    languages
+  }) {
+    // TODO: test this method.
+    return this._put('api/update', {
+      username,
+      id,
+      name,
+      category,
+      remake,
+      description,
+      status,
+      hidden,
+      website_link: websiteLink,
+      languages
+    })
+  }
+
+  /**
+   * Search for torrents.
+   * @param {Array<string>} c - In which categories to search.
+   * @param {string} q - Query to search (torrent name).
+   * @param {number} page - Page of the search results.
+   * @param {string} limit - Number of results per page.
+   * @param {string} userID - Uploader ID owning the torrents.
+   * @param {string} fromID - Show results with torrents ID superior to this.
+   * @param {string} s - Torrent status.
+   * @param {string} maxage - Torrents which have been uploaded the last x days.
+   * @param {string} toDate - Torrents which have been uploaded since x
+   * <code>dateType</code>.
+   * @param {string} fromDate - Torrents which have been uploaded the last x
+   * <code>dateType</code>.
+   * @param {string} dateType - Which type of date (<code>d</code> for days,
+   * <code>m</code> for months, <code>y</code> for years).
+   * @param {string} minSize - Filter by minimal size in <code>sizeType</code>.
+   * @param {string} maxSize - Filter by maximal size in <code>sizeType</code>.
+   * @param {string} sizeType - Which type of size (<code>b</code> for bytes,
+   * <code>k</code> for kilobytes, <code>m</code> for megabytes, <code>g</code>
+   * for gigabytes).
+   * @param {string} sort - Torrent sorting type (0 = id, 1 = name, 2 = date,
+   * 3 = downloads, 4 = size, 5 = seeders, 6 = leechers, 7 = completed).
+   * @param {boolean} order - Order ascending or descending (true = ascending).
+   * @param {Array<string>} lang - Filter the languages.
+   * @param {number} page - Search page.
+   * @return {Promise<QueryResponse, Error>} - The promise to search for a list
+   * of torrents.
+   */
+  search({
+    c,
+    q,
+    page,
+    limit,
+    userID,
+    fromID,
+    s,
+    maxage,
+    toDate,
+    fromDate,
+    dateType,
+    minSize,
+    maxSize,
+    sizeType,
+    sort,
+    order,
+    lang
+  } = {}) {
+    if (typeof sort === 'string') {
+      sort = this._sorters[sort]
+    }
+
+    return this._get('api/search', {
+      c,
+      q,
+      page,
+      limit,
+      userID,
+      fromID,
+      s,
+      maxage,
+      toDate,
+      fromDate,
+      dateType,
+      minSize,
+      maxSize,
+      sizeType,
+      sort,
+      order,
+      lang
+    })
+  }
+
+  /**
+   * Login with a username and password.
+   * @param {string} username - Username or Email.
+   * @param {string} password - Password.
+   * @returns {Promise<Profile, Error>} - The promise to login with a username
+   * and password.
+   */
+  login({username, password}) {
+    return this._post('api/login', {
+      username,
+      password
+    })
+  }
+
+  /**
+   * Get a profile based on the given id.
+   * @param {Number} id - User ID.
+   * @return {Promise<Profile, Error} - The promise ot get a profile.
+   */
+  getProfile(id) {
+    return this._get(`api/profile`, { id })
+  }
+
+  // XXX: Endpoint is not on production yet.
+  // /**
+  //  * Get the current use of the api token.
+  //  * @return {Promise<Object, Error>} - The promise to get a user.
+  //  */
+  // getUser() {
+  //   return this._get(`api/user`)
   // }
 
-  // update() {
-  //   return this._request('PUT', '/api/update')
-  // }
+  /**
+   * TODO: add description.
+   * @return {Promise<Object, Error>} - The promise to refresh the API token.
+   */
+  refreshToken() {
+    return this._get('api/token/refresh')
+  }
+
+  /**
+   * TODO: add description.
+   * @return {Promise<Object, Error>} - The promise to check the API token.
+   */
+  checkToken() {
+    return this._get('api/token/check')
+  }
 
 }
--- test/nyaa.js
+++ test/nyaa.js
@@ -1,97 +1,225 @@
 'use strict'
 
+// Import the necessary modules.
 const { expect } = require('chai')
-const NyaaAPI = require('../nyaa-api-pt')
+const NyaaApi = require('../nyaa-api-pt')
 
-describe('Nyaa', () => {
-
+/** @test {NyaaApi} */
+describe('NyaaApi', () => {
+  /**
+ * The NyaaApi instance.
+ * @type {NyaaApi}
+ */
   let nyaa
 
+  /**
+   * Hook for setting up the NyaaApi tests.
+   * @type {Function}
+   */
   before(() => {
+    // Disable the warn logging function to testing.
     console.warn = () => {}
-    nyaa = new NyaaAPI({
+
+    // TODO: add apiToken to encrypted env variables.
+    nyaa = new NyaaApi({
+      apiToken: process.env.NYAA_TOKEN,
       debug: true
     })
   })
 
+  /**
+   * Test the torrent attributes.
+   * @param {Torrent} torrent - The torrent to test.
+   * @returns {undefined}
+   */
+  function testTorrentAttributes(torrent) {
+    expect(torrent).to.be.an('object')
+    expect(torrent.id).to.be.a('number')
+    expect(torrent.name).to.be.a('string')
+    expect(torrent.status).to.be.a('number')
+    expect(torrent.hash).to.be.a('string')
+    expect(torrent.date).to.be.a('string')
+    expect(torrent.filesize).to.be.a('number')
+    expect(torrent.description).to.be.a('string')
+    expect(torrent.comments).to.be.an('array')
+    expect(torrent.sub_category).to.be.a('string')
+    expect(torrent.category).to.be.a('string')
+    expect(torrent.anidb_id).to.be.a('string')
+    expect(torrent.uploader_id).to.be.a('number')
+    expect(torrent.uploader_name).to.be.a('string')
+    expect(torrent.uploader_old).to.be.a('string')
+    expect(torrent.website_link).to.be.a('string')
+    expect(torrent.languages).to.be.an('array')
+    expect(torrent.magnet).to.be.a('string')
+    expect(torrent.torrent).to.be.a('string')
+    expect(torrent.seeders).to.be.a('number')
+    expect(torrent.leechers).to.be.a('number')
+    expect(torrent.completed).to.be.a('number')
+    expect(torrent.last_scrape).to.be.a('string')
+    expect(torrent.file_list).to.be.an('array')
+  }
+
+  /** @test {NyaaApi#getTorrent} */
+  it('should get a torrent', done => {
+    nyaa.getTorrent(962988).then(res => {
+      testTorrentAttributes(res)
+      done()
+    }).catch(done)
+  })
+
+  /** @test {NyaaApi#getTorrentHead} */
+  it('should get a torrent head', done => {
+    // TODO: fix this test.
+
+    // For branch coverage.
+    nyaa = new NyaaApi({
+      apiToken: process.env.NYAA_TOKEN
+    })
+    nyaa.getTorrentHead(962988).then(res => {
+      expect(res).to.be.a('string')
+      // testTorrentAttributes(res)
+      done()
+    }).catch(done)
+  })
+
+  /** @test {NyaaApi#uploadTorrent} */
+  it('should upload a torrent', done => {
+    // TODO: fix this test.
+
+    // nyaa.uploadTorrent({
+    //   username,
+    //   name,
+    //   magnet,
+    //   category,
+    //   remake,
+    //   description,
+    //   status,
+    //   hidden,
+    //   websiteLink,
+    //   languages,
+    //   torrent
+    // }).then(res => {
+    //   expect(res).to.be.an('object')
+    //   done()
+    // }).catch(done)
+
+    done()
+  })
+
+  /** @test {NyaaApi#updateTorrent} */
+  it('should update a torrent', done => {
+    // TODO: fix this test.
+
+    // nyaa.updateTorrent({
+    //   username: process.env.NYAA_USER,
+    //   id,
+    //   name: 'test torrent',
+    //   category: '3_5',
+    //   remake: true,
+    //   description: 'jist a test torrent',
+    //   status: 0,
+    //   hidden: 'tru',
+    //   // websiteLink,
+    //   languages: 'en'
+    // }).then(res => {
+    //   expect(res).to.be.an('object')
+    //   done()
+    // }).catch(done)
+
+    done()
+  })
+
+  /** @test {NyaaApi#search} */
   it('should search for torrents', done => {
     nyaa.search({
-      query: 'horriblesubs',
-      filter: 'trusted_only',
-      category: 'anime',
-      sub_category: 'english_translated',
-      page: 2,
-      sort: 'date',
-      order: false,
-      max: 300,
-      userID: 14
+      q: 'horriblesubs',
+      page: 1,
+      limit: 50,
+      sort: 'id'
     }).then(res => {
-      expect(res).to.be.an('array')
-      expect(res.length).to.be.at.least(1)
-
-      done()
-    }).catch(done)
-  })
-
-  it('should throw an error when searching for torrents', () => {
-    // expect(nyaa.search.bind(nyaa.search, {
-    //   filter: 'failing'
-    // })).to.throw('failing is an invalid option for filter!')
-    expect(nyaa.search.bind(nyaa.search, {
-      sort: 'failing'
-    })).to.throw('failing is an invalid option for sort!')
-    expect(nyaa.search.bind(nyaa.search, {
-      category: 'failing'
-    })).to.throw('failing is an invalid option for category!')
-    expect(nyaa.search.bind(nyaa.search, {
-      subCategory: 'failing'
-    })).to.throw('failing')
-
-    expect(nyaa.search.bind(nyaa.search, {
-      category: null,
-      subCategory: 'failing'
-    })).to.throw('Sub category needs a category!')
-  })
-
-  it('should get torrents from a page', done => {
-    nyaa.getTorrents({
-      max: 50,
-      page: 1
-    }).then(res => {
+      expect(res).to.be.an('object')
       expect(res.torrents).to.be.an('array')
-      expect(res.torrents.length).to.be.at.least(1)
-      expect(res.queryRecordCount).to.be.a('number')
-      expect(res.totalRecordCount).to.be.a('number')
+
+      const random = Math.floor(Math.random() * res.torrents.length)
+      testTorrentAttributes(res.torrents[random])
 
       done()
     }).catch(done)
   })
 
-  it('should get details on a torrent', done => {
-    nyaa.getTorrent(924727).then(res => {
-      expect(res.id).to.be.a('number')
-      expect(res.name).to.be.a('string')
-      expect(res.hash).to.be.a('string')
-      expect(res.magnet).to.be.a('string')
+  /** @test {NyaaApi#login} */
+  it('should login with a username and password', done => {
+    // TODO: add username and password to encrypted env variables.
+    nyaa.login({
+      username: process.env.NYAA_USER,
+      password: process.env.NYAA_PASS
+    }).then(res => {
+      expect(res).to.be.an('object')
+      // expect(res.infos).to.be.an('array')
+      expect(res.ok).to.be.a('boolean')
+
+      const { data } = res
+      expect(data).to.be.an('object')
+      expect(data.user_id).to.be.an('number')
+      expect(data.username).to.be.an('string')
+      expect(data.status).to.be.an('number')
+      expect(data.md5).to.be.an('string')
+      expect(data.created_at).to.be.an('string')
+      expect(data.liking_count).to.be.an('number')
+      expect(data.liked_count).to.be.an('number')
 
       done()
     }).catch(done)
   })
 
-  // it('should upload a torrent', () => {
-  //   nyaa.upload({
-  //     name,
-  //     category,
-  //     subCategory,
-  //     description,
-  //     hash
-  //   }).then(res => {
+  /** @test {NyaaApi#getProfile} */
+  it('should get a profile by the id', done => {
+    nyaa.getProfile(13803).then(res => {
+      expect(res).to.be.an('object')
+      // expect(res.infos).to.be.an('array')
+      expect(res.ok).to.be.a('boolean')
+
+      const { data } = res
+      expect(data).to.be.an('object')
+      expect(data.user_id).to.be.an('number')
+      expect(data.username).to.be.an('string')
+      expect(data.status).to.be.an('number')
+      expect(data.md5).to.be.an('string')
+      expect(data.created_at).to.be.an('string')
+      expect(data.liking_count).to.be.an('number')
+      expect(data.liked_count).to.be.an('number')
+
+      done()
+    }).catch(done)
+  })
+
+  // XXX: Endpoint is not on production yet.
+  // /** @test {NyaaApi#getUser} */
+  // it('should get a user of the api key', done => {
+  //   nyaa.getUser().then(res => {
+  //     expect(res).to.be.an('object')
   //     done()
   //   }).catch(done)
   // })
-  //
-  // it('should update a torrent', () => {
-  //
-  // })
 
+  /** @test {NyaaApi#refreshToken} */
+  it('should refresh the api token', done => {
+    //
+    nyaa.refreshToken().then(res => {
+      // TODO: fix this test.
+
+      expect(res).to.be.an('object')
+      done()
+    }).catch(done)
+  })
+
+  /** @test {NyaaApi#checkToken} */
+  it('should check the api token', done => {
+    // TODO: fix this test.
+
+    nyaa.checkToken().then(res => {
+      expect(res).to.be.an('object')
+      done()
+    }).catch(done)
+  })
 })
